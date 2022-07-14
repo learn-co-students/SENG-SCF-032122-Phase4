366f3f3b4d0094bc21f2536a48dd37d10fb9a0fa
Shelby Talbert <shelby.talbert@flatironschool.com>
2022-07-14 14:44:34 -0400

    code is ready for lecture

---
+++ 03-client-server-comms-p2/flatiron-theater-api/app/controllers/productions_controller.rb
@@ -0,0 +1,33 @@
+class ProductionsController < ApplicationController
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
+    
+    def index 
+        productions = Production.all
+        render json: productions
+    end 
+
+    def show
+        production = Production.find(params[:id])
+        render json: production
+    end 
+
+    def create
+        production = Production.create!(production_params)
+        render json: production, status: :created
+    end 
+
+
+
+
+    private 
+
+    def production_params
+        params.permit(:title, :genre, :description, :budget, :image, :director, :ongoing, :id)
+    end 
+
+    def render_unprocessable_entity(invalid)
+        render json: {errors: invalid.record.errors}, status: :unprocessable_entity 
+    end
+
+
+end
---
+++ 03-client-server-comms-p2/flatiron-theater-api/config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  # Not using routes right now, comment back in as we create new routes
+  # resources :users
+  # resources :tickets
+  # resources :production_roles
+  resources :productions, only: [:index, :show, :create]
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  # if we need to write a custom route here is a template
+  # get '/productions', to: 'productions#index'
+end
